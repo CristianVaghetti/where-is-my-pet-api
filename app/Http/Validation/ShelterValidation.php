2d801223f626590a8a858684373396c13c549e21
Cristian Vaghetti <vaghetticristian@gmail.com>
2024-09-30 10:00:10 -0300

    finalizado a solicitação e aprovação de gerenciamento do abrigo, já com a nova estrutura

--- app/Http/Validation/ShelterValidation.php
+++ app/Http/Validation/ShelterValidation.php
@@ -4,28 +4,21 @@ namespace App\Http\Validation;
 
 use App\Helpers\ResponseHelper;
 use App\Http\Validation\IValidation;
+use App\Models\ShelterManagementRequest;
 use App\Repository\ShelterRepository;
-use Illuminate\Support\Arr;
-
+Use Illuminate\Support\Facades\Auth;
 class ShelterValidation implements IValidation
 {
-    /**
-     * Repository of Shelter
-     *
-     * @var ShelterRepository
-     */
-    protected ShelterRepository $repository;
-
     /**
      * Create a new validation instance.
      *
      * @param ShelterRepository $repository
      * @return void
      */
-    public function __construct(ShelterRepository $repository)
-    {
-        $this->repository = $repository;
-    }
+    public function __construct(
+        protected ShelterRepository $repository, 
+        protected ShelterManagementRequest $smrequest
+    ) {}
 
     /**
      * Make a busines validate
@@ -39,4 +32,31 @@ class ShelterValidation implements IValidation
         return ResponseHelper::responseSuccess(json: false);
     }
 
+    public function validateMRequest(int $id = 0)
+    {
+        $shelter = $this->repository->find($id);
+        if (!$shelter) return ResponseHelper::responseValidateError(msg: 'Abrigo não localizado!', json: false);
+
+        $smrequest = $this->smrequest->where(['shelter_id' => $id, 'user_id' => Auth::user()->id])->whereNull('viewed_at')->first();
+        if ($smrequest) return ResponseHelper::responseValidateError(msg: 'Já existe uma solicitação em aberto!', json: false);
+
+        # só pro intelephense não ficar incomodando
+        /** @var \App\User $user */ 
+        $user = Auth::user();
+        $userShelter = $user->shelters()->where('shelter_id', $id)->first();
+        if ($userShelter) return ResponseHelper::responseValidateError(msg: 'Você já pode gerenciar este abrigo!', json: false);
+
+        return ResponseHelper::responseSuccess(json: false);
+    }
+
+    public function validateMApproval(array $data)
+    {
+        $smrequest = $this->smrequest->find($data['id']);
+        if (!$smrequest) return ResponseHelper::responseValidateError(msg: 'Solicitação não localizada!', json: false);
+
+        $shelter = $smrequest->shelter;
+        if (!$shelter->my_shelter) return ResponseHelper::responseValidateError(msg: 'Você não é o responsável por este abrigo!', json: false);
+
+        return ResponseHelper::responseSuccess(json: false);
+    }
 }
--- app/Services/ShelterService.php
+++ app/Services/ShelterService.php
@@ -3,30 +3,24 @@
 namespace App\Services;
 
 use App\Helpers\ResponseHelper;
+use App\Http\Validation\ShelterValidation;
 use App\Models\ShelterManagementRequest;
 use App\Repository\ShelterRepository;
-use Illuminate\Support\Facades\Auth;
 
 class ShelterService
 {
     public function __construct(
         protected ShelterRepository $shelterRepository,
-        protected ShelterManagementRequest $smrequest
+        protected ShelterManagementRequest $smrequest,
+        protected ShelterValidation $validation,
     ) {}
 
     public function managementRequest(int $id)
     {
+        $validation = $this->validation->validateMRequest($id);
+        if(!$validation['success']) return $validation;
+
         $shelter = $this->shelterRepository->find($id);
-        if (!$shelter) return ResponseHelper::responseError(msg: 'Abrigo não localizado!');
-
-        $smrequest = $this->smrequest->where(['shelter_id' => $id, 'user_id' => Auth::user()->id])->whereNull('viewed_at')->first();
-        if ($smrequest) return ResponseHelper::responseError(msg: 'Já existe uma solicitação em aberto!');
-
-        # só pro intelephense não ficar incomodando
-        /** @var \App\User $user */ 
-        $user = Auth::user();
-        $userShelter = $user->shelters()->where('shelter_id', $id)->first();
-        if ($userShelter) return ResponseHelper::responseError(msg: 'Você já pode gerenciar este abrigo!');
 
         if ($this->shelterRepository->managementRequest($shelter->id)) {
             return ResponseHelper::responseSuccess(msg: 'Sucesso!');
@@ -37,11 +31,10 @@ class ShelterService
 
     public function managementApproval(array $data = [])
     {
-        $smrequest = $this->smrequest->find($data['id']);
-        if (!$smrequest) return ResponseHelper::responseValidateError(msg: 'Solicitação não localizada!');
+        $validation = $this->validation->validateMApproval($data);
+        if(!$validation['success']) return $validation;
 
-        $shelter = $smrequest->shelter;
-        if (!$shelter->my_shelter) return ResponseHelper::responseValidateError(msg: 'Você não é o responsável por este abrigo!');
+        $smrequest = $this->smrequest->find($data['id']);
 
         if ($this->shelterRepository->managementApproval($smrequest, $data['approved'])) {
             return ResponseHelper::responseSuccess(msg: 'Sucesso!');
